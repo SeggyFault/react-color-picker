69b991e9fa36a412ad2bf9e0f77bf44c0c232de7
sggyflt <sgflt88@gmail.com>
2024-09-02 17:18:53 +0200

    feat: add ColorPicker component functionality

--- src/ColorPicker/ColorPicker.jsx
+++ src/ColorPicker/ColorPicker.jsx
@@ -4,9 +4,18 @@ import { useState } from "react";
 function ColorPicker() {
 
   const [color, setColor] = useState("#FFFFFF");
+  function handleColorChange(e) {
+    setColor(e.target.value);
+  }
+
   return(
-    <div className={styles.colorPicker}>
+    <div className={styles.colorPickerContainer}>
       <h1>Color Picker</h1>
+      <div className={styles.colorDisplay} style={{backgroundColor: color}}>
+        <p>Selected Color: {color}</p>
+      </div>
+      <label>Select a Color:</label>
+      <input type="color" value={color} onChange={handleColorChange}/>
     </div>
   )
 }
