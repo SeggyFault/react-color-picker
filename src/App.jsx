f601650f70160e0c40b3fd7ec35bc2b6dc232aba
sggyflt <sgflt88@gmail.com>
2024-09-02 16:54:27 +0200

    feat: add ColorPicker component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import ColorPicker from "./ColorPicker/ColorPicker.jsx";  
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <ColorPicker/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,9 @@
+function ColorPicker() {
+  return(
+    <div>
+      <h1>Color Picker</h1>
+    </div>
+  )
+}
+
+export default ColorPicker;
